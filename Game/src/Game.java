262c500176dfc50bc35d95dfcef4e053d02c9f46
sahilTHUKRAL <sahilt222@gmail.com>
2018-05-29 00:57:10 +0530

    Basic Implimentation
    Two initial Player are given choose 0\white\a player and get first move 
    conditions checked-
    1.Check before and after move.
    2.CheckMate
    3.StaleMate

---
+++ Game/src/Game.java
@@ -0,0 +1,51 @@
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class Game{
+	
+public static void main(String A[])throws Exception{
+	 Charset utf8Charset = Charset.forName("UTF-8");
+	    Charset defaultCharset = Charset.defaultCharset();
+	Player a = new Player();
+	Player b = new Player();
+	 Board chessBoard = new Board(a,b);
+	a.move(11, 2);
+	 b.move(12, 2);
+	 a.move(10,1);
+	 chessBoard.print();
+
+	
+	 //game loop
+	 while(a.checkForMoveLeft() && b.checkForMoveLeft()) {
+		 System.out.println(a.pieceSet.colour+"'s move");
+		 a.move();
+	 //chessBoard.turnTheBoard();
+	 chessBoard.print();
+		 System.out.println(b.pieceSet.colour+"'s move");
+		 b.move();
+	 chessBoard.print();
+}
+	 if(!a.moveLeft)
+	 {
+		 if(a.underCheck) {
+		 System.out.println(" Player B wins");
+		 }
+		 else {
+			 System.out.println("Match Draw By staleMate");
+		 }
+	 }
+	 if(!b.moveLeft)
+	 {
+		 if(b.underCheck) {
+		 System.out.println(" Player A wins");
+		 }
+		 else {
+			 System.out.println("Match Draw By staleMate");
+		 }
+	 }
+}
+public static boolean GameAlive(Player a, Player b) {
+	if(a.checkForMoveLeft() && b.checkForMoveLeft()) {return true;}
+	return false;
+}
+}
